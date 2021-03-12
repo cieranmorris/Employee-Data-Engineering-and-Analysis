46c3444b096e1ca6b1e9f9ea70260c9685448849
Ciera Morris <cieranmorris@gmail.com>
2021-03-11 18:42:15 -0700

    working on table development

--- Analysis/employees_db_analysis.sql
+++ Analysis/employees_db_analysis.sql
@@ -16,3 +16,9 @@ CREATE TABLE employees(
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE titles(
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
