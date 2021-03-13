57b24251a314a489009c75a04ed2e3aa80f819c4
Ciera Morris <cieranmorris@gmail.com>
2021-03-12 20:48:51 -0700

    second query accomplished

--- Analysis/queries.sql
+++ Analysis/queries.sql
@@ -7,4 +7,11 @@ employees.sex AS "Sex",
 salaries.salary AS "Salary"
 FROM employees
 LEFT JOIN salaries on 
-employees.emp_no = salaries.emp_no
+employees.emp_no = salaries.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986
+SELECT employees.first_name AS "First Name",
+employees.last_name AS "Last Name",
+employees.hire_date AS "Hire Date"
+FROM employees
+WHERE hire_date >= '01/01/1986' AND hire_date < '12/31/1986';
