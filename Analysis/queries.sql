b517549f7c3c5ced42d0eaa6b155ee7c31f19212
Ciera Morris <cieranmorris@gmail.com>
2021-03-12 21:16:36 -0700

    troubleshooted third query

--- Analysis/queries.sql
+++ Analysis/queries.sql
@@ -15,3 +15,13 @@ employees.last_name AS "Last Name",
 employees.hire_date AS "Hire Date"
 FROM employees
 WHERE hire_date >= '01/01/1986' AND hire_date < '12/31/1986';
+
+--List the manager of each department incorporating department number, department name, the manager's employee number, last name, first name
+SELECT departments.dept_no AS "Department Number",
+departments.dept_name AS "Department Name",
+dept_manager.emp_no AS "Manager Employee Number",
+employees.last_name AS "Last Name",
+employees.first_name AS "First Name"
+FROM dept_manager
+INNER JOIN employees ON dept_manager.emp_no = employees.emp_no
+INNER JOIN departments ON dept_manager.dept_no = departments.dept_no;
